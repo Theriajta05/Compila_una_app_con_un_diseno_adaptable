ccf9098df413b98b989968155392f423468339c0
Theriajta05 <118383201+Theriajta05@users.noreply.github.com>
2024-10-22 14:57:08 -0600

    Vistas previas para los tamaños

--- basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.reply
 
 import android.os.Bundle
@@ -65,14 +51,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 1000)
 @Composable
-fun ReplyAppCompactPreview() {
+fun ReplyAppExpandedPreview() {
     ReplyTheme {
         Surface {
-            ReplyApp(
-                windowSize = WindowWidthSizeClass.Compact,
-            )
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
         }
     }
 }
